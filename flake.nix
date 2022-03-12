3d9f86349d65b3142889c9e3979f168166db78e5
hugosenari <hugosenari@gmail.com>
2022-03-12 00:10:57 -0300

    chore: re index nimbleimgui

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''ImGui Frontend for Nimble'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimbleimgui-master".url = "path:./master";
+  inputs."nimbleimgui-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimbleimgui-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''ImGui Frontend for Nimble'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-NimbleImGui-master.flake = false;
+  inputs.src-NimbleImGui-master.owner = "qb-0";
+  inputs.src-NimbleImGui-master.ref   = "refs/heads/master";
+  inputs.src-NimbleImGui-master.repo  = "NimbleImGui";
+  inputs.src-NimbleImGui-master.type  = "github";
+  
+  inputs."nimgl".dir   = "nimpkgs/n/nimgl";
+  inputs."nimgl".owner = "riinr";
+  inputs."nimgl".ref   = "flake-pinning";
+  inputs."nimgl".repo  = "flake-nimble";
+  inputs."nimgl".type  = "github";
+  inputs."nimgl".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgl".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-NimbleImGui-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-NimbleImGui-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
